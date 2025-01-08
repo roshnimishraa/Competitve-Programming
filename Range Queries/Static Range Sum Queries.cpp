896863d00d1785f781b1ed8787147326e9c92844
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2025-01-08 16:49:51 +0530

    Update Static Range Sum Queries.cpp

--- Range Queries/Static Range Sum Queries.cpp
+++ Range Queries/Static Range Sum Queries.cpp
@@ -28,3 +28,83 @@ int main() {
 
     return 0;
 }
+
+
+Approach 2: Segment Tree
+
+Time Complexity:
+Build: O(n) (each element contributes to O(logn) operations, summing up to O(n))
+Query: O(logn) per query, as the tree height is logn.
+    
+Space Complexity: O(4n)
+    
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+#define ll long long
+ 
+void buildSegmentTree(vector<ll> &segmentTree, vector<ll> &arr, ll node, ll low, ll high)
+{
+    if(low == high)
+    {
+        segmentTree[node] = arr[low];
+    }
+    else{
+        ll mid = (low + high)/2;
+        ll leftChild = 2*node + 1;
+        ll rightChild = 2*node + 2;
+        
+        buildSegmentTree(segmentTree, arr, leftChild, low, mid);
+        buildSegmentTree(segmentTree, arr, rightChild, mid+1, high);
+        
+        segmentTree[node] = segmentTree[leftChild] + segmentTree[rightChild];
+        
+    }
+}
+ 
+ll querySegment(vector<ll> &tree, ll node, ll start, ll end, ll l, ll r)
+{
+ if(r < start || l>end)
+ {
+     return 0;
+ }
+ 
+ if(l<=start && end<=r)
+ {
+     return tree[node];
+ }
+ 
+   ll mid = (start + end)/2;
+   ll leftChild = 2*node + 1;
+   ll rightChild = 2*node + 2;
+   
+   ll leftSum = querySegment(tree, leftChild, start, mid, l, r);
+   ll rightSum = querySegment(tree, rightChild, mid+1, end, l, r);
+        
+return leftSum + rightSum;
+}
+ 
+int main()
+{
+    ll n,q;
+    cin>>n>>q;
+    vector<ll> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    vector<ll> segmentTree(4*n);
+    buildSegmentTree(segmentTree, arr, 0, 0, n-1);
+    
+    while(q--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        a--;  // Convert to 0-based indexing
+        b--;
+        
+    cout<<querySegment(segmentTree, 0, 0, n-1, a, b)<<endl;
+    }
+    return 0;
+}
+
